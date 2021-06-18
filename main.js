6bff44a0d9cb4430fce3ef454afd8f292f452a9a
SaitejasAnand <82390705+SaitejasAnand@users.noreply.github.com>
2021-06-18 19:21:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+menu_list_array = ["Veg Margherita Pizza",//add more items
+"Veg tandoori pizza","Veg sup[reme pizza","Veg paneer tikka pizza","Veg extravaganza pizza","Veg farmhouse pizza","Garlic bread","Veg pepperoni pizza","Veg Pacific coast vegie pizza","Desi veg pizza"                  ];
+,"Italian pizza"
+
+menu_list_array = ["coca cola",//add more items "pepsi","Thumbs up","Fanta","sprite"
+
+menu_list_array.sort();
+
+function getmenu(){
+var htmldata;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
+
